71e674dfbaf979f4965ef024e8c7fba4b8736cee
tessium <ekzobayt@gmail.com>
2021-05-24 15:45:45 +0500

    update

--- books/views.py
+++ books/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+from books.models import BookModel
 
 
 def index(request):
+    q = request.GET.get('q')
+
+    if q:
+        data = BookModel.objects.filter(Q(title__icontains=q) | Q(author__name__icontains=q)).order_by('-pk')
+    else:
+        data = BookModel.objects.all()
+
+    context = {
+        'data': data
+    }
     return render(request, 'index.html', context)
+
+
+def detail(request, pk=5):
+    # try:
+    #     book = BookModel.objects.get(pk=pk)
+    # except BookModel.DoesNotExist:
+    #     raise Http404
+
+    book = get_object_or_404(BookModel, pk=pk)
+
+    context = {
+        'book': book
+    }
+    return render(request, 'detail.html', context)
--- djangoProject4/urls.py
+++ djangoProject4/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from books.views import index
+from books.views import index, detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', index)
+    path('books/', index),
+    path('books/<int:pk>/', detail)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
